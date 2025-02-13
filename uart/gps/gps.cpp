fe7189bd4d69bc6c39bdd787745608604e1c1615
Umut Özil <80857139+umut373@users.noreply.github.com>
2025-02-14 02:47:29 +0300

    refactor: simplify gps event handling switch-case structure

--- uart/gps/gps.cpp
+++ uart/gps/gps.cpp
@@ -6,8 +6,6 @@
 
 using namespace gps;
 
-static const char *TAG = "uart_events";
-
 GPS::GPS() {
     uart_config_t uart_config = {
         .baud_rate = 9600,
@@ -99,57 +97,25 @@ void GPS::uart_event_task_entry_point(void* obj) {
 }
 
 void GPS::uart_event_task(uart_event_t event, uint8_t* data) {
-    if (xQueueReceive(this->uart_queue, (void *)&event, (TickType_t)portMAX_DELAY)) {
+    if (xQueueReceive(uart_queue, (void *)&event, (TickType_t)portMAX_DELAY)) {
         bzero(data, BUF_SIZE);
-        switch (event.type) {
-            case UART_DATA:
-                ESP_LOGI(TAG, "[UART DATA]: %d", event.size);
-                uart_read_bytes(UART_NUM, data, event.size, portMAX_DELAY);
-                ESP_LOGI(TAG, "[DATA EVT]: %s", data);
-                break;
-            case UART_FIFO_OVF:
+        if (event.type == UART_PATTERN_DET) {
+            int pos = uart_pattern_pop_pos(UART_NUM);
+            if (pos == -1) {
                 uart_flush_input(UART_NUM);
-                xQueueReset(this->uart_queue);
-                break;
-            case UART_BUFFER_FULL:
-                uart_flush_input(UART_NUM);
-                xQueueReset(this->uart_queue);
-                break;
-            case UART_BREAK:
-                ESP_LOGI(TAG, "uart rx break");
-                break;
-            case UART_FRAME_ERR:
-                ESP_LOGI(TAG, "uart frame error");
-                break;
-            case UART_PARITY_ERR:
-                ESP_LOGI(TAG, "uart parity error");
-                break;
-            case UART_DATA_BREAK:
-                ESP_LOGI(TAG, "uart rx data break");
-                break;
-            case UART_PATTERN_DET: {
-                int pos = uart_pattern_pop_pos(UART_NUM);
-                if (pos == -1) {
-                    uart_flush_input(UART_NUM);
-                } else {
-                    uint8_t* pattern = new uint8_t[PATTERN_CHR_NUM+1];
-                    uart_read_bytes(UART_NUM, data, pos-1, pdMS_TO_TICKS(100));
-                    uart_read_bytes(UART_NUM, pattern, PATTERN_CHR_NUM+1, pdMS_TO_TICKS(100));
-                    delete[] pattern;
+            } else {
+                uint8_t* pattern = new uint8_t[PATTERN_CHR_NUM+1];
+                uart_read_bytes(UART_NUM, data, pos-1, pdMS_TO_TICKS(100));
+                uart_read_bytes(UART_NUM, pattern, PATTERN_CHR_NUM+1, pdMS_TO_TICKS(100));
+                delete[] pattern;
 
-                    if (strncmp((char*)data, "$GPGGA", 6) == 0 && check_NMEA_data(data, pos-1)) {
-                        parse_NMEA_data(data);
-                    }
+                if (strncmp((char*)data, "$GPGGA", 6) == 0 && check_NMEA_data(data, pos-1)) {
+                    parse_NMEA_data(data);
                 }
-                break;
             }
-            case UART_EVENT_MAX:
-                ESP_LOGI(TAG, "uart event max");
-                break;
-
-            default:
-                ESP_LOGI(TAG, "uart event type: %d", event.type);
-                break;
+        } else if (event.type == UART_FIFO_OVF || event.type == UART_BUFFER_FULL) {
+            uart_flush_input(UART_NUM);
+            xQueueReset(uart_queue);
         }
     }
 }
